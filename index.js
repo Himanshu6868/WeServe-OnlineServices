0a48f1ac23d5b3341ac099f07f741b88bfdf6361
himanshu <himanshuyadav3519@gmail.com>
2023-08-10 13:52:13 +0530

    changes

---
+++ index.js
@@ -0,0 +1,14 @@
+function myFunction() {
+  const element = document.querySelector(".login-form");
+  element.scrollIntoView({behavior: "smooth", block: "start", inline: "start"});
+}
+
+function aboutFunction(){
+  const el = document.querySelector(".services-section");
+  el.scrollIntoView({behavior: "smooth", block: "start", inline: "start"});
+}
+
+function mediaFunction(){
+  const el = document.querySelector(".credits");
+  el.scrollIntoView({behavior: "smooth", block: "start", inline: "start"});
+}
